4e9e6d8eda877b11b23b922ee4340df1b82905de
Christiano0407 <placebo0704@gmail.com>
2022-03-09 13:10:00 -0600

    header-new

---
+++ src/js/index.js
@@ -0,0 +1,21 @@
+console.log("Blog Web Animation");
+// Grab Element >
+const selectElement = (selector) => {
+  const element = document.querySelector(selector);
+  if (element) return element;
+  throw new Error(
+    `Something went wrong! Make sure that ${selector} exists/is typed correctly.`
+  );
+};
+
+//> Nav style on Scroll ===>
+const scrollHeader = () => {
+  const navbarElement = selectElement(`#header`);
+  if (this.scrollY >= 15) {
+    navbarElement.classList.add("activated");
+  } else {
+    navbarElement.classList.remove("activated");
+  }
+};
+
+window.addEventListener("scroll", scrollHeader);
