ddbccc59f98329894e82b64ac4e455c85762afec
Christiano0407 <placebo0704@gmail.com>
2022-03-28 13:00:32 -0600

    add awesome

---
+++ blog/js/appBlog.js
@@ -0,0 +1,8 @@
+//** ==== Blog Web Javascript  === */
+//**Import Swiper  >>> */
+//import { Swiper } from "swiper";
+//**Import Swiper Styles >>>  */
+//import "swiper/css";
+
+//**Create your swiper >>>  */
+//const Swiper = new Swiper();
