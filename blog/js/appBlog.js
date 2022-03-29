61dda01c0aeec46404111076152bf1709ea38cc8
Christiano0407 <placebo0704@gmail.com>
2022-03-29 12:15:09 -0600

    navigation

--- blog/js/appBlog.js
+++ blog/js/appBlog.js
@@ -6,3 +6,28 @@
 
 //**Create your swiper >>>  */
 //const Swiper = new Swiper();
+
+//** ==== Blog Web Javascript  === */
+//** // Grab elements y Podremos seleccionar por medio del Element*/
+const selectElement = (selector) => {
+  const element = document.querySelector(selector);
+  if (element) return element;
+  throw new Error(
+    `Something went wrong! Make sure that ${selector} exists/is typed correctly.`
+  );
+};
+
+//** = Nav styles on scroll =  ====== =====> */
+const scrollHeader = () => {
+  const navBarElement = selectElement("#headerId");
+
+  if (this.scrollY >= 15) {
+    navBarElement.classList.add("activated");
+  } else {
+    navBarElement.classList.remove("activated");
+  }
+};
+
+window.addEventListener("scroll", scrollHeader);
+
+//* Open menu & search pop-up >> === >>> */
